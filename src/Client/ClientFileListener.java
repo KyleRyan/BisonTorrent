7fdb3ba81f8abaa16d14ee65b9e46934f62e5c49
joshuaphilippy <joshuaphilippy@gmail.com>
2016-03-21 18:30:21 -0500

    added P2P threads

---
+++ src/Client/ClientFileListener.java
@@ -0,0 +1,36 @@
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author philippy
+ */
+public class ClientFileListener extends Thread {
+
+    public ClientFileListener() {
+    }
+    ServerSocket serverSocket = null;
+
+    public void run() {
+        try {
+            serverSocket = new ServerSocket(10005);
+            System.out.println("Started on: ");
+        } catch (IOException e) {
+        }
+
+        while (true) {
+            try {
+                new ClientFileSend(serverSocket.accept()).start();
+            } catch (IOException ex) {
+            }
+        }
+    }
+}
---
+++ src/Client/ClientFileSend.java
@@ -0,0 +1,48 @@
+package Client;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.DatagramSocket;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class ClientFileSend extends Thread {
+
+    private Socket socket = null;
+    private static final int BUFFER_SIZE = 32768;
+
+    public ClientFileSend(Socket socket) {
+        super("Send");
+        this.socket = socket;
+    }
+
+    public void run() {
+        DataOutputStream out = null;
+        BufferedReader in = null;
+
+        try {
+
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+            String fileName = "";
+            try {
+                while ((fileName = in.readLine()) != null) {
+                }
+            } catch (Exception e) {
+            }
+            System.out.println(fileName);
+
+            socket.close();
+        } catch (IOException ex) {
+            Logger.getLogger(ClientFileSend.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
