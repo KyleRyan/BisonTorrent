17c0885fa81cd054d26fa4c84deaef223991ce5b
JPisaBrony <jdpohl789@gmail.com>
2016-03-22 00:02:44 -0500

    + clearing filelist

--- src/Client/ClientForm.java
+++ src/Client/ClientForm.java
@@ -121,6 +121,7 @@ public class ClientForm extends javax.swing.JFrame {
     	String text = jTextField1.getText();
     	for(InetAddress peer : peerList)
     		new ClientFileReceive(text, peer).start();
+    	listModel.clear();
     }
 
     /**
